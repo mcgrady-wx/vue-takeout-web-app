bc3a9a60db142af2f13802f7378335185491aab3
wen <380198622@qq.com>
2020-10-23 16:18:40 +0800

    底部导航完成

--- src/components/FooterNav/FooterNav.vue
+++ src/components/FooterNav/FooterNav.vue
@@ -1,7 +1,30 @@
 <template>
     <!-- 底部导航 -->
-    <div>
-        FooterNav
+    <div class="footer_guide">
+        <router-link to="/home" class="guide_item">
+             <span class="item_icon">
+                <i class="iconfont icon-U"></i>
+            </span>
+            <span>外卖</span>
+        </router-link>
+        <router-link to="/search" class="guide_item">
+             <span class="item_icon">
+                <i class="iconfont icon-sousuo1"></i>
+            </span>
+            <span>搜索</span>
+        </router-link>
+        <router-link to="/order" class="guide_item">
+             <span class="item_icon">
+                <i class="iconfont icon-dingdan1"></i>
+            </span>
+            <span>订单</span>
+        </router-link>
+        <router-link to="/profile" class="guide_item">
+             <span class="item_icon">
+                <i class="iconfont icon-geren"></i>
+            </span>
+            <span>我的</span>
+        </router-link>
     </div>
 </template>
 
@@ -12,5 +35,33 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+    @import '../../common/stylus/mixins.styl'
+    .footer_guide  //footer
+        top-border-1px(#e4e4e4)
+        position fixed
+        z-index 100
+        left 0
+        right 0
+        bottom 0
+        background-color #fff
+        width 100%
+        height 50px
+        display flex
+        .guide_item
+            display flex
+            flex 1
+            text-align center
+            flex-direction column
+            align-items center
+            margin 5px
+            color #999999
+            &.router-link-active
+                color #02a774
+            span
+                font-size 12px
+                margin-top 2px
+                margin-bottom 2px
+                .iconfont
+                    font-size 22px
 
 </style>
